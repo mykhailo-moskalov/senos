38c3356abedfd6408920b41b29d026b02499a56f
mishokkartoshki <misha@moskalov.com>
2025-05-17 19:37:55 +0200

    footer & form

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,16 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  const modal = document.querySelector('[data-modal]');
+  const modalBgOverlay = document.querySelector('[data-modal-bg]');
+  document
+    .querySelectorAll(
+      '[data-modal-open], [data-modal-close], [data-modal-bg-open], [data-modal-bg-close]'
+    )
+    .forEach(btn => {
+      btn.addEventListener('click', toggleModal);
+    });
+  modalBgOverlay?.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle('is-open');
+    modal.classList.toggle('is-open');
+    modalBgOverlay?.classList.toggle('is-open');
   }
 })();
